556a6bf3762edae631e8c6d2b6df51edf680f70d
PedroOrPeter <pedro.henrique_maciel@hotmail.com>
2023-02-23 23:00:06 -0300

    Labels and Entrys

--- tkinterExe.py
+++ tkinterExe.py
@@ -9,6 +9,9 @@ class AppVoiceRecognition:
         self.root = root
         self.screen()
         self.frames_screen()
+        self.button()
+        self.labels_entrys_first()
+        self.labels_entrys_second()
         root.mainloop()
 
     def screen(self):
@@ -26,6 +29,74 @@ class AppVoiceRecognition:
         self.frame_second = Frame(self.root, bd=6, highlightbackground='black', highlightthickness=2)
         self.frame_first.place(relx=0.1, rely=0.06, relheight=0.4, relwidth=0.8)
         self.frame_second.place(relx=0.1, rely=0.5, relheight=0.4, relwidth=0.8)
+
+    def button(self):
+        self.button_enter_first = Button(self.frame_first, bg='green', text='Enter', font=('Arial', 15))
+        self.button_enter_second = Button(self.frame_second, bg='green', text='Enter', font=('Arial', 15))
+        self.button_enter_first.place(relx=0.78, rely=0.78, relheight=0.2, relwidth=0.2)
+        self.button_enter_second.place(relx=0.78, rely=0.78, relheight=0.2, relwidth=0.2)
+
+    def labels_entrys_first(self):
+        self.lb_nome = Label(self.frame_first, text= "Nome:")
+        self.lb_nome.place(relx=0.05, rely=0.05)
+
+        self.nome_entry = Entry(self.frame_first)
+        self.nome_entry.place(relx=0.1, rely=0.05, relwidth=0.15)
+
+        self.lb_sobrenome = Label(self.frame_first, text="Sobrenome:")
+        self.lb_sobrenome.place(relx=0.05, rely=0.2)
+
+        self.sobrenome_entry = Entry(self.frame_first)
+        self.sobrenome_entry.place(relx=0.134, rely=0.2, relwidth=0.15)
+
+        self.lb_idade = Label(self.frame_first, text="Idade:")
+        self.lb_idade.place(relx=0.35, rely=0.05)
+
+        self.idade_entry = Entry(self.frame_first)
+        self.idade_entry.place(relx=0.4, rely=0.05, relwidth=0.15)
+
+        self.lb_email = Label(self.frame_first, text="Email:")
+        self.lb_email.place(relx=0.35, rely=0.2)
+
+        self.email_entry = Entry(self.frame_first)
+        self.email_entry.place(relx=0.4, rely=0.2, relwidth=0.15)
+
+        self.lb_motivo = Label(self.frame_first, text="Motivo:")
+        self.lb_motivo.place(relx=0.05, rely=0.4)
+
+        self.motivo_entry = Entry(self.frame_first)
+        self.motivo_entry.place(relx=0.11, rely=0.4, relwidth=0.7, relheight=0.3)
+
+    def labels_entrys_second(self):
+        self.lb_nome = Label(self.frame_second, text="Nome:")
+        self.lb_nome.place(relx=0.05, rely=0.05)
+
+        self.nome_entry = Entry(self.frame_second)
+        self.nome_entry.place(relx=0.1, rely=0.05, relwidth=0.15)
+
+        self.lb_sobrenome = Label(self.frame_second, text="Sobrenome:")
+        self.lb_sobrenome.place(relx=0.05, rely=0.2)
+
+        self.sobrenome_entry = Entry(self.frame_second)
+        self.sobrenome_entry.place(relx=0.134, rely=0.2, relwidth=0.15)
+
+        self.lb_idade = Label(self.frame_second, text="Idade:")
+        self.lb_idade.place(relx=0.35, rely=0.05)
+
+        self.idade_entry = Entry(self.frame_second)
+        self.idade_entry.place(relx=0.4, rely=0.05, relwidth=0.15)
+
+        self.lb_email = Label(self.frame_second, text="Email:")
+        self.lb_email.place(relx=0.35, rely=0.2)
+
+        self.email_entry = Entry(self.frame_second)
+        self.email_entry.place(relx=0.4, rely=0.2, relwidth=0.15)
+
+        self.lb_motivo = Label(self.frame_second, text="Motivo:")
+        self.lb_motivo.place(relx=0.05, rely=0.4)
+
+        self.motivo_entry = Entry(self.frame_second)
+        self.motivo_entry.place(relx=0.11, rely=0.4, relwidth=0.7, relheight=0.3)
 
 
 AppVoiceRecognition()
