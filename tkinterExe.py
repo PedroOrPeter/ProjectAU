c6cdd4ce131f9ad54f572d8e4174d64998f61c8b
pedro.maciel.golivetech <pedro.maciel.golivetech@gmail.com>
2023-02-23 21:27:36 -0300

    Screen properties

--- tkinterExe.py
+++ tkinterExe.py
@@ -3,14 +3,22 @@ from tkinter import *
 
 root = Tk()
 
-class AppVoiceRecognition():
+
+class AppVoiceRecognition:
     def __init__(self):
         self.root = root
         self.tela()
         root.mainloop()
 
     def tela(self):
+        # Title
         self.root.title('Voice Recognition')
+        # Configure
         self.root.configure(background='black')
+        # Screen size when it starts
+        self.root.geometry('1080x720')
+        # Not able to screen resize
+        self.root.resizable(False, False)
+
 
 AppVoiceRecognition()
