0962b6e772f8e83bfee98ce30c0856787ada0353
PedroOrPeter <pedro.henrique_maciel@hotmail.com>
2023-02-24 21:33:05 -0300

    Funcionalidade falar+printar

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ rec = sr.Recognizer()
 print(sr.Microphone().list_microphone_names())
 device = int(input('De acordo com o indice da variavel dentro da lista, selecione o dispositivo que deseja usar:'))
 with sr.Microphone(device_index=device) as microfone:
+    rec.adjust_for_ambient_noise(microfone)
+    audio = rec.listen(microfone)
 
-
+    texto = rec.recognize_google(audio, language="pt-BR")
+    print(texto)
