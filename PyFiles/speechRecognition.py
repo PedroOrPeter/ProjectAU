549108f54d470aa45a8845b3f361314b616c4c41
PedroOrPeter <pedro.henrique_maciel@hotmail.com>
2023-02-25 16:39:23 -0300

    Refractor Directorys

--- PyFiles/speechRecognition.py
+++ PyFiles/speechRecognition.py
@@ -1,12 +1,13 @@
 import speech_recognition as sr
 
 
-class voiceRecognition:
+class VoiceRecognition:
     def __init__(self):
         self.microphone = sr.Microphone()
         self.rec = sr.Recognizer()
         self.show_devices()
-        self.listening_voice(self.select_devices(), self.rec)
+        texto = self.listening_voice(self.select_devices(), self.rec)
+        self.show_text(texto)
 
     def show_devices(self):
         print(sr.Microphone().list_microphone_names())
@@ -18,10 +19,14 @@ class voiceRecognition:
     def listening_voice(self, device, rec):
         with sr.Microphone(device_index=device) as microphone:
             rec.adjust_for_ambient_noise(microphone)
+            rec.pause_threshold = 2
             audio = rec.listen(microphone)
+            texto = rec.recognize_google(audio, language="pt-br")
+        return texto
 
-            texto = rec.recognize_google(audio, language="en-us")
-            print(texto)
+    def show_text(self, texto):
+        print(texto)
 
-voiceRecognition()
+
+VoiceRecognition()
 
