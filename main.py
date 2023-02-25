8f0e9c32114d774b8d8c63c82583fc8cd565d771
PedroOrPeter <pedro.henrique_maciel@hotmail.com>
2023-02-24 21:06:17 -0300

    Mic imp

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import speech_recognition as sr
-from tkinterExe import *
+
+rec = sr.Recognizer()
+
+print(sr.Microphone().list_microphone_names())
+device = int(input('De acordo com o indice da variavel dentro da lista, selecione o dispositivo que deseja usar:'))
+with sr.Microphone(device_index=device) as microfone:
 
 
