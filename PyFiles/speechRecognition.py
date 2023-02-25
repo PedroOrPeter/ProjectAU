2da8b54eed491d27148db5df626f814215b767e7
PedroOrPeter <pedro.henrique_maciel@hotmail.com>
2023-02-25 17:04:27 -0300

    Class SpeechRec

--- PyFiles/speechRecognition.py
+++ PyFiles/speechRecognition.py
@@ -6,26 +6,30 @@ class VoiceRecognition:
         self.microphone = sr.Microphone()
         self.rec = sr.Recognizer()
         self.show_devices()
-        texto = self.listening_voice(self.select_devices(), self.rec)
+        texto = self.listening(self.select_devices())
         self.show_text(texto)
 
     def show_devices(self):
-        print(sr.Microphone().list_microphone_names())
+        print(self.microphone.list_microphone_names())
 
     def select_devices(self):
         device = int(input('Choose which device you want to record audio from: (Based in index, eg: 0, 1, 2...)'))
         return device
 
-    def listening_voice(self, device, rec):
+    def listening(self, device):
         with sr.Microphone(device_index=device) as microphone:
-            rec.adjust_for_ambient_noise(microphone)
-            rec.pause_threshold = 2
-            audio = rec.listen(microphone)
-            texto = rec.recognize_google(audio, language="pt-br")
+            # Treating noise
+            self.rec.adjust_for_ambient_noise(microphone)
+            # Speech Max Pause
+            self.rec.pause_threshold = 2
+            # Listening
+            audio = self.rec.listen(microphone)
+            # Saving audio to a variable
+            texto = self.rec.recognize_google(audio, language="en-US")
         return texto
 
     def show_text(self, texto):
-        print(texto)
+        print('Resultado: ', texto)
 
 
 VoiceRecognition()
