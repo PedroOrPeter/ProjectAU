631481503d1e6223c9f6f6d07e1d16d569c4d40e
PedroOrPeter <pedro.henrique_maciel@hotmail.com>
2023-02-25 18:20:23 -0300

    Imp Audio W

--- PyFiles/speechRecognition.py
+++ PyFiles/speechRecognition.py
@@ -6,13 +6,15 @@ class VoiceRecognition:
         self.microphone = sr.Microphone()
         self.rec = sr.Recognizer()
         self.show_devices()
-        texto = self.listening(self.select_devices())
-        self.show_text(texto)
+        audio = self.listening(self.select_devices())
+        self.writing_audio(audio)
+        self.save_audio(audio)
 
     def show_devices(self):
-        print(self.microphone.list_microphone_names())
+        print(self.microphone.list_working_microphones())
 
     def select_devices(self):
+        # Saving device
         device = int(input('Choose which device you want to record audio from: (Based in index, eg: 0, 1, 2...)'))
         return device
 
@@ -24,13 +26,17 @@ class VoiceRecognition:
             self.rec.pause_threshold = 2
             # Listening
             audio = self.rec.listen(microphone)
-            # Saving audio to a variable
-            texto = self.rec.recognize_google(audio, language="en-US")
-        return texto
+        return audio
 
-    def show_text(self, texto):
+    def writing_audio(self, audio):
+        # Saving audio to a variable
+        texto = self.rec.recognize_google(audio, language="en-US")
+        # Print result
         print('Resultado: ', texto)
+
+    def save_audio(self, texto):
+        with open('audio.wav', 'w') as arquivo:
+            arquivo.write(texto.get)
 
 
 VoiceRecognition()
-
