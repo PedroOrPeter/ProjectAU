ac7519610f269d97161ff2cdfc8a5ac044ae9f5d
PedroOrPeter <pedro.henrique_maciel@hotmail.com>
2023-02-27 00:13:59 -0300

    REF Speech

--- PyFiles/speechRecognition.py
+++ PyFiles/speechRecognition.py
@@ -3,42 +3,46 @@ import speech_recognition as sr
 
 class VoiceRecognition:
     def __init__(self):
+        self.device = None
+        self.audio = None
+        self.text = None
         self.microphone = sr.Microphone()
         self.rec = sr.Recognizer()
         self.show_devices()
-        device = self.select_devices()
-        audio = self.listening(device)
-        self.writing_audio(audio)
-        self.save_audio(audio)
+        self.select_devices()
+        self.listening()
+        self.writing_audio()
+        self.save_audio()
 
     def show_devices(self):
         print(self.microphone.list_working_microphones())
 
     def select_devices(self):
         # Saving device
-        device = int(input('Choose which device you want to record audio from: (Based in index, eg: 0, 1, 2...)'))
-        return device
+        self.device = int(input('Choose which device you want to record audio from: (Based in index, eg: 0, 1, 2...)'))
+        return self.device
 
-    def listening(self, device):
-        with sr.Microphone(device_index=device) as microphone:
+    def listening(self):
+        with sr.Microphone(device_index=self.device) as microphone:
             # Treating noise
             self.rec.adjust_for_ambient_noise(microphone)
             # Speech Max Pause
             self.rec.pause_threshold = 1
             # Listening
-            audio = self.rec.listen(microphone)
-        return audio
+            self.audio = self.rec.listen(microphone)
+        return self.audio
 
-    def writing_audio(self, audio):
+    def writing_audio(self):
         # Saving audio to a variable
-        text = self.rec.recognize_google(audio, language="en-US")
+        self.text = self.rec.recognize_google(self.audio, language="en-US")
         # Print result
-        print('Result: ', text)
+        print('Result: ', self.text)
+        return self.text
 
-    def save_audio(self, text):
+    def save_audio(self):
         # Writing a wav file in binaries
         with open('audio.wav', 'wb') as file:
-            file.write(text.get_wav_data())
+            file.write(self.text.get_wav_data())
 
 
 VoiceRecognition()
