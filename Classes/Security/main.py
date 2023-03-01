268be80e3ce07cff67b9950e99ea4ea08529339e
PedroOrPeter <pedro.henrique_maciel@hotmail.com>
2023-02-28 23:59:43 -0300

    Error

--- Classes/Security/main.py
+++ Classes/Security/main.py
@@ -4,26 +4,25 @@ from CmdControl import *
 
 
 root = GetIpByDevice()
-target = root
 
 
 def capture_ip_device():
-    target.get_hostname()
+    root.get_hostname()
     if target:
-        target.get_ip_address()
+        root.get_ip_address()
 
 
 def wrt_read_txt():
     try:
-        target.open_txt()
-        target.write_txt()
-        target.close_txt()
+        root.open_txt()
+        root.write_txt()
+        root.close_txt()
     except PermissionError:
         print(err)
 
 
 def hide_file():
-    target.hide_files()
+    root.hide_files()
 
 
 if __name__ == '__main__':
